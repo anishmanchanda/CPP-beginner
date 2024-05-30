dea1aeb15870898b620e7315ba0d7cf4754278ce
Anish Manchanda <anishmanchanda2006@gmail.com>
2024-05-30 14:33:01 +0530

    --

--- maxof3nos.cpp
+++ maxof3nos.cpp
@@ -17,18 +17,18 @@ int main(){
     
     if(num1>num2){
         if(num1>num3){
-            cout<<num1;
+            cout<<num1<<endl;
         }
         else{
-            cout<<num3;
+            cout<<num3<<endl;
         }
     }
     else{
         if(num2>num3){
-            cout<<num2;
+            cout<<num2<<endl;
         }
         else{
-            cout<<num3;
+            cout<<num3<<endl;
         }
     }
     
