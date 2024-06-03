33c4bb73e84f2b90c2bc7d2f98a85b157ca68b4f
Anish Manchanda <anishmanchanda2006@gmail.com>
2024-06-03 13:45:00 +0530

    divisible program

---
+++ divisibe.cpp
@@ -0,0 +1,21 @@
+//prgram to check whether entered number is divisble by 2 and/or 3
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter number: ";
+    cin>>n;
+    if (n%2==0 && n%3==0){
+        cout<<"divisible by both 2 and 3"<<endl;
+    }
+    else if(n%2==0){
+        cout<<"divisble by 2 only"<<endl;
+    }
+    else if(n%3==0){
+        cout<<"divisible by 3 only"<<endl;
+    }
+    else{
+        cout<<"not divisible by both 2 and 3"<<endl;
+    }
+
+}
