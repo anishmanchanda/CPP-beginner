55abbe8699874e3e3129916f1d354cd95e773fa0
Anish Manchanda <anishmanchanda2006@gmail.com>
2024-06-04 12:21:35 +0530

    patterns

---
+++ Patterns.cpp
@@ -0,0 +1,26 @@
+//patterns with c++
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    //RECTANGLE
+
+    /*
+    int length,breadth;
+    cout<<"enter length";
+    cin>>length;
+    cout<<"enter breadth";
+    cin>>breadth;
+    string star="* ";
+    for(int i=1;i<=length;i++){
+        for(int j=1;j<=breadth;j++){
+            cout<<star;
+
+        } 
+        cout<<endl;
+    }
+    */
+
+   
+}
