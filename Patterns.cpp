8a02d322c73587d657236516797b1285eacdcc77
Anish Manchanda <anishmanchanda2006@gmail.com>
2024-06-04 12:57:55 +0530

    hollow rectangle

--- Patterns.cpp
+++ Patterns.cpp
@@ -22,5 +22,30 @@ int main(){
     }
     */
 
-   
-}
+
+   //HOLLOW RECTANGLE
+
+   int rows,collumns;
+   cout<<"enter no. of rows";
+   cin>>rows;
+   cout<<"enter no. of collumns";
+   cin>>collumns;
+   string star="* ";
+   for(int rowno=1;rowno<=rows;++rowno){
+        for(int colno=1;colno<=collumns;++colno){
+            if(rowno==1 or rowno==rows){
+                cout<<star;
+            }
+            else{
+                if(colno==1 or colno==collumns){
+                    cout<<star;
+                }
+                else{
+                    cout<<" ";
+                }
+            }
+            }
+        cout<<endl;
+        }
+
+}    
