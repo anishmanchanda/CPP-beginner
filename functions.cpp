47c61bc61c2fccae7a637fcea50ea0a0c5f1bbc2
Anish Manchanda <anishmanchanda2006@gmail.com>
2024-06-08 02:56:26 +0530

    sum of n nos.

--- functions.cpp
+++ functions.cpp
@@ -3,9 +3,22 @@ using namespace std;
 
 //sum of n natural numbers
 int sum(int n){
+    int sum=0;
+    for(int i=1;i<=n;i++){
+        sum+=i;
+    }
+    return sum;
+}
+
+bool pythtrip(int x, int y, int z){
+    int a=max(x,max(y,z)); 
     
 }
 
-int main(){
 
+int main(){
+    int n;
+    cout<<"enter n";
+    cin>>n;
+    cout<<sum(n)<<endl;
 }
