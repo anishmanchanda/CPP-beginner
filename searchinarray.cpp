757f511931305f867ec555132d703aa61803a47d
Anish Manchanda <anishmanchanda2006@gmail.com>
2024-06-12 23:22:45 +0530

    --

---
+++ searchinarray.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+//program to return index of element stored in array
+int main(){
+    cout<<"enter no. of elements: ";
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cout<<"enter element";
+        cin>>arr[i];
+    }
+    cout<<"enter element to search: ";
+    
+    for(int j=0;j<n;j++){
+
+    }
+}
