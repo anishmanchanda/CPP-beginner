4817627e3d37e94d79558290898f4631c76bb80f
Anish Manchanda <anishmanchanda2006@gmail.com>
2024-06-13 19:56:58 +0530

    using func search

--- searchinarray.cpp
+++ searchinarray.cpp
@@ -1,18 +1,29 @@
 #include <iostream>
 using namespace std;
-//program to return index of element stored in array
+//program to return index of element stored in array using func
+
+int indexsearch(int arr[],int n,int key){
+    for (int i=0;i<n;i++){
+        if(arr[i]==key){
+            return i;
+            break; 
+        }
+    }
+    return -1;
+}
 int main(){
     cout<<"enter no. of elements: ";
     int n;
     cin>>n;
     int arr[n];
     for(int i=0;i<n;i++){
-        cout<<"enter element";
+        cout<<"enter element: ";
         cin>>arr[i];
     }
     cout<<"enter element to search: ";
+    int key;
+    cin>>key;  
+    cout<<"index of entered no.: "<<indexsearch(arr,n,key)<<endl;
+    return 0; 
     
-    for(int j=0;j<n;j++){
-
-    }
 }
