2e52878e8f343087e3238bdc7b6d8ee50f456571
Anish Manchanda <anishmanchanda2006@gmail.com>
2024-06-18 19:28:59 +0530

    --

---
+++ sum-of-allsubarrays.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<"enter number of elements: ";
+    int n;
+    cin>>n;
+    int array[n];
+    for(int i=0;i<n;i++){
+        cout<<"enter elements: ";
+        cin>>array[i];
+    }
+    int sum=0;
+    for(int i=0;i<n;i++){
+        for(int j=i;j<n;j++){
+            sum+=array[]
+        }
+    }
+}
