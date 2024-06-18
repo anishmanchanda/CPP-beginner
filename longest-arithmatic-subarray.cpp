99cfbb90d7adf427ad893ee68d4d5b863557c5ce
Anish Manchanda <anishmanchanda2006@gmail.com>
2024-06-19 00:08:06 +0530

    incomplete

---
+++ longest-arithmatic-subarray.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<"enter number of elements: ";
+    int n;
+    cin>>n;
+    int array[n];
+    for(int i=0;i<n;i++){
+        cout<<"enter elements: ";
+        cin>>array[i];
+    }
+    for(int =)
+    return 0;
+}
