a8b89830ed6c9dd1f32787eb79e2bb183bc47806
Anish Manchanda <anishmanchanda2006@gmail.com>
2024-06-19 12:33:01 +0530

    longest arithmatic subarray

--- longest-arithmatic-subarray.cpp
+++ longest-arithmatic-subarray.cpp
@@ -1,3 +1,5 @@
+//program to find the length of longest arithmatic subarray in an array.
+//arithmetic subarray: elements forming AP(same common difference)
 #include <iostream>
 using namespace std;
 int main(){
@@ -9,6 +11,17 @@ int main(){
         cout<<"enter elements: ";
         cin>>array[i];
     }
-    for(int =)
+    int length=1;
+    int pd=9999999;
+    for(int i=1;i<=n;i++){
+        int a=array[i];
+        int b=array[i-1];
+        if(pd==(a-b)){
+            length+=1;
+        }
+        pd=a-b;
+
+    }
+    cout<<length<<" elements"<<endl;
     return 0;
 }
