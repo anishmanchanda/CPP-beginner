c76902ee78c81a9abffb73b817d1913c638b8631
Anish Manchanda <anishmanchanda2006@gmail.com>
2024-07-01 20:28:52 +0530

    matrix transpose🎉

---
+++ transpose-of-matrix.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,m;
+    cout<<"enter no. of rows: ";
+    cin>>n;
+    cout<<"enter no. of collumns: ";
+    cin>>m;
+    int arr[n][m];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cout<<"enter element: ";
+            cin>>arr[i][j];
+        }
+        cout<<endl;
+    }
+    cout<<"Original matrix:- "<<endl;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cout<<arr[j][i]<<" ";
+        }
+        cout<<endl;
+    }
+    
+    return 0;
+}
