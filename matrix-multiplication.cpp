7202b9ba2fcc49c6b5c09a14359f71d87f5486fb
Anish Manchanda <anishmanchanda2006@gmail.com>
2024-07-02 20:33:16 +0530

    matrix multiplicaiton🎉

--- matrix-multiplication.cpp
+++ matrix-multiplication.cpp
@@ -1,37 +1,51 @@
 #include <iostream>
-
 using namespace std;
 int main(){
-    cout<<"enter data for matrix 1:-"<<endl;
-    int row1,col1;
-    cout<<"enter no. of rows: ";
+    int row1,col1row2,col2;
+    cout<<"enter n rows for 1st: ";
     cin>>row1;
-    cout<<"enter no. of collumns: ";
-    cin>>col1;
-    int arr1[row1][col1];
+    cout<<"enter ncol1 and row2: ";
+    cin>>col1row2;
+    cout<<"enter n cols for 2nd: ";
+    cin>>col2;
+    //1st array
+    int arr1[row1][col1row2];
     for(int i=0;i<row1;i++){
-        for(int j=0;j<col1;j++){
+        for(int j=0;j<col1row2;j++){
             cout<<"enter element: ";
             cin>>arr1[i][j];
         }
         cout<<endl;
     }
-    cout<<"enter data for matrix 2:-"<<endl;
-    int row2,col2;
-    cout<<"enter no. of rows: ";
-    cin>>row2;
-    cout<<"enter no. of collumns: ";
-    cin>>col2;
-    int arr[row2][col2];
-    for(int i=0;i<row2;i++){
+    //2nd array
+    int arr2[col1row2][col2];
+    for(int i=0;i<col1row2;i++){
         for(int j=0;j<col2;j++){
-            cout<<"enter element: ";
-            cin>>arr[i][j];
+            cout<<"enter elemnt: ";
+            cin>>arr2[i][j];
         }
         cout<<endl;
     }
-    //multiplication of entered matrices
-    
-    
+    //multiplication of 1st and 2nd
+    int ans[row1][col2];
+    for(int i=0;i<row1;i++){
+        for(int j=0;j<col2;j++){
+            ans[i][j]=0;
+        }
+    }
+    for(int i=0;i<row1;i++){
+        for(int j=0;j<col2;j++){
+            for(int k=0;k<col1row2;k++){
+                ans[i][j]+=arr1[i][k]*arr2[k][j];
+            }
+        }
+    }
+    cout<<"product matrix:-"<<endl;
+    for(int i=0;i<row1;i++){
+        for(int j=0;j<col2;j++){
+            cout<<ans[i][j]<<" ";
+        }
+        cout<<endl;
+    }
     return 0;
 }
