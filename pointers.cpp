2718cd59e344ae8ab34504c5ddc086662a2d6762
Anish Manchanda <anishmanchanda2006@gmail.com>
2024-07-05 21:41:16 +0530

    pointers

--- pointers.cpp
+++ pointers.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
 using namespace std;
 int main(){
-    
+    int a=10;
+    int*aptr;// int* because int type variable. if bool type then bool*aptr;
+    aptr=&a;
+    cout<<&a<<endl;
+    cout<<aptr<<endl;
+    cout<<*aptr<<endl; //will print value of a
+    //*aptr is original vaerile (a)
+    //&a is address of a
+    *aptr=20;
+    cout<<a<<endl;
+    return 0;
 }
