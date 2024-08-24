3f404c97b15020dba2570c8ab4bb52e549483da2
Anish Manchanda <anishmanchanda2006@gmail.com>
2024-08-24 13:42:07 +0530

    checking reachablity of locations

---
+++ checking-reachability.cpp
@@ -0,0 +1,4 @@
+//finding whether path to a certain coordinate in an array exists or not
+//make an array with i rows and j collumns having 0 and 1 as values. 
+//iff arr[i][j]==1 then j is reachable from i. if arr[i][j]==0 then j is not reachable from i.
+
