7a1c347cadb62b17445a8ea3c0e44120d324c789
Anish Manchanda <anishmanchanda2006@gmail.com>
2024-09-07 20:05:37 +0530

    reachable or not

--- checking-reachability.cpp
+++ checking-reachability.cpp
@@ -2,3 +2,11 @@
 //make an array with i rows and j collumns having 0 and 1 as values. 
 //iff arr[i][j]==1 then j is reachable from i. if arr[i][j]==0 then j is not reachable from i.
 
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter number of coordinates: ";
+    cin>>n;
+    
+}
