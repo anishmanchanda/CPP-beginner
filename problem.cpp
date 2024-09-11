60d2ab27175631fd284d4d56d50056ef4586d585
Anish Manchanda <anishmanchanda2006@gmail.com>
2024-09-11 20:30:56 +0530

    prob

---
+++ problem.cpp
@@ -0,0 +1,22 @@
+//Simple Calculator
+#include <iostream>
+using namespace std;
+int main(){
+    bool flag=true;
+    int count=1;
+    int num,n;
+
+    while(flag==true){
+        
+        cout<<"Hello World "<<count<<endl;
+        count+=1;
+        if(count>6){
+            flag=false;
+        }
+    }
+    void func(){
+        while(){
+            
+        }
+    }
+}
