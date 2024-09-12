a8ab84b648c2f45b8ae0448cc642a332a6aee240
Anish Manchanda <anishmanchanda2006@gmail.com>
2024-09-12 10:00:20 +0530

    progress

--- problem.cpp
+++ problem.cpp
@@ -1,22 +1,30 @@
 //Simple Calculator
 #include <iostream>
 using namespace std;
+
+int func(int numin){
+    int n1,n2;
+    cin>>n1;
+    cin>>n2;
+    if (numin==1){
+        return n1+n2;
+    }
+    else if (numin==2){
+        return n1-n2;
+    }
+    else if(numin==3){
+        
+    }
+}
 int main(){
     bool flag=true;
-    int count=1;
     int num,n;
-
+    int arr[15]
+    int count=0;
     while(flag==true){
-        
-        cout<<"Hello World "<<count<<endl;
+        cin>>num;
+        if(num)
+        arr[count]=func(num);
         count+=1;
-        if(count>6){
-            flag=false;
-        }
-    }
-    void func(){
-        while(){
-            
-        }
     }
 }
