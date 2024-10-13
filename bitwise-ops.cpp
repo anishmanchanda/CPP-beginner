da79125f0bf57f0e92b04b4b8360fe97506a41e5
Anish Manchanda <anishmanchanda2006@gmail.com>
2024-10-14 01:18:50 +0530

    bitwise operators in functions

--- bitwise-ops.cpp
+++ bitwise-ops.cpp
@@ -2,9 +2,20 @@
 using namespace std;
 
 int getBit(int n, int pos){
-     return n;
+     return (n&(1<<pos)!=0);
 }
-
+int setBit(int n, int pos){
+    return (n|(1<<pos));
+}
+int clearBit(int n,int pos){
+    int mask=~(1<<pos);
+    return (n & mask);
+}
+int updateBit(int n,int pos,int value){//to update value at a certain position in a given binary no.
+    int mask=~(1<<pos);
+    n=n=n&mask;
+    return (n|(value<<pos));
+}
 int main(){
     return 0;
 }
--- test.cpp
+++ test.cpp
@@ -2,7 +2,5 @@
 #include <array>
 using namespace std;
 int main(){
-    int array[]={10,20,30,40,50};
-    int n=sizeof(array)/sizeof(array[0]);
-    cout<<n<<endl;
+    cout<<(4&5)!=0<<endl;
 }
