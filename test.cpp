974c259c4dd17c13d9f0e5a968a5bff2c7f2c726
Anish Manchanda <anishmanchanda2006@gmail.com>
2024-11-18 22:02:01 +0530

    random

--- test.cpp
+++ test.cpp
@@ -1,6 +1,19 @@
 #include<iostream>
 #include <array>
 using namespace std;
+
+double areaSwitchCase(int ch, vector<double> a) {
+  switch (ch) {
+  case 1:
+    cout<<3.14*a[0]*a[0];
+    return 3.14*a[0]*a[0];
+  case 2:
+	cout<<a[0]*a[1];
+    return a[0]*a[1];
+  }
+
+}
+
 int main(){
-    cout<<(4&5)!=0<<endl;
+    areaSwitchCase(2,{3,2});
 }
