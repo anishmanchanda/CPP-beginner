ad404efafd4daa6abfd61b0b03f69d25d11da234
Anish Manchanda <anishmanchanda2006@gmail.com>
2024-11-27 01:11:43 +0530

    ---

--- test.cpp
+++ test.cpp
@@ -1,7 +1,8 @@
 #include<iostream>
 #include <array>
+#include <climits>
 using namespace std;
-
+/*
 double areaSwitchCase(int ch, vector<double> a) {
   switch (ch) {
   case 1:
@@ -13,7 +14,33 @@ double areaSwitchCase(int ch, vector<double> a) {
   }
 
 }
-
+*/
 int main(){
-    areaSwitchCase(2,{3,2});
+    //areaSwitchCase(2,{3,2});
+    int a[4]={3,4,5,2};
+    int max=INT_MIN;
+    int max2=INT_MIN;
+    int min=INT_MAX;
+    int min2=INT_MAX;
+    int el;
+    for(int i=0;i<5;i++){
+        el=a[i];
+        if(el<min){
+            min=el;
+        }
+        if(el>max){
+            max=el;
+        }
+    } 
+    for(int i=0;i<5;i++){
+        el=a[i];
+        if(el>min && el<min2){
+            min2=el;
+        }
+
+        if(el<max && el>max2){
+            max2=el;
+        }
+    } 
+    cout<<max2<<" "<<min2<<endl;
 }
