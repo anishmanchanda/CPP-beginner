31b4eda0f8ce609a7ca18ce98453f2ef67f19b41
Anish Manchanda <anishmanchanda2006@gmail.com>
2025-01-25 01:06:55 +0530

    sum-of-n-nos-recursion

---
+++ sum-of-n-nos-recursion.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int sum(int n){
+    if(n==0){
+        return 0;
+    }
+    int prevsum=sum(n-1);
+    return n + prevsum;
+
+}
+int  main(){
+    int n;
+    cin>>n;
+    cout<<"sum upto n is: "<<sum(n)<<endl;
+    return 0;
+}
