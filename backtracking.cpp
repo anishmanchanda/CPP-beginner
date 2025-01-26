a3fbb709589fe3e0108d29d2f678c7b4339862ba
Anish Manchanda <anishmanchanda2006@gmail.com>
2025-01-26 23:57:36 +0530

    -back-rat-in-maze

---
+++ backtracking.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+//algorithmic technique for solving recursive problems
+//rat in maze: 2 options at each point: right or down
+int main(){
+    //1=rat can go, 2=rat can't go
+    
+    //input
+    int maze[][]={{1,0,1,0,1},{1,1,1,1,1},{0,1,0,1,0},{1,0,0,1,1},{1,1,1,0,1}};
+
+    
+
+    return 0;
+}
