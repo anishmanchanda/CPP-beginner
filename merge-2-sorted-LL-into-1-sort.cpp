bdfdc8cd05b52bcc48f4213a454c3da27e0b42fb
Anish Manchanda <anishmanchanda2006@gmail.com>
2025-02-26 22:35:43 +0530

    trash-clean

---
+++ merge-2-sorted-LL-into-1-sort.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <linked-list.h>
+using namespace std;
+Node* mergeSorted(Node* head1, Node* head2){
+    //compare last and first elements of both lists
+    Node* temp1=head1;
+    Node* temp2=head2;
+    while(temp1!=NULL){
+        temp1=temp1->next;
+    }
+    cout<<temp1
+    if((temp1->val)>(temp2->val)){
+        
+    }
+    return head1;
+}
+int main(){
+    
+    return 0;
+}
